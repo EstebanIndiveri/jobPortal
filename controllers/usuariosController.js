fbf8134275bd0b7a52f8d9505df6b175325bdc10
Esteban Indiveri <esteban.indiveri@gmail.com>
2020-12-26 11:26:26 -0300

    prefix auth email user

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,29 +1,16 @@
 const Usuarios=require('../models/Usuarios');
-
 exports.formCrearCuenta=(req,res,next)=>{
     res.render('crear-cuenta',{
         nombrePagina:'Crear tu cuenta en devJobos',
-        tagline:'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta'
+        tagline:'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta',
     })
-}
-exports.crearUsuario=async(req,res,next)=>{
-    const usuario=new Usuarios(req.body);
-    try {
-        await usuario.save();
-        res.redirect('/iniciar-sesion');
-        req.flash('error', 'Ese correo ya esta registrado')
-    } catch (error) {
-        req.flash('error', 'Ese correo ya esta registrado')
-        res.redirect('/crear-cuenta');
-    }
 }
 exports.validarRegistro=(req,res,next)=>{
     // sanitizar campos mutabilidaa
     req.sanitizeBody('nombre').escape();
     req.sanitizeBody('email').escape();
-    // req.sanitizeBody('password').escape();
-    // req.sanitizeBody('confirmar').escape();
-    // console.log(req.body);
+    req.sanitizeBody('password').escape();
+    req.sanitizeBody('confirmar').escape();
     // valida
     req.checkBody('nombre','El nombre es obligatorio').notEmpty();
     req.checkBody('email','El email debe ser valido').isEmail();
@@ -32,9 +19,7 @@ exports.validarRegistro=(req,res,next)=>{
     req.checkBody('confirmar','El password es diferente').equals(req.body.password);
 
     const errores=req.validationErrors();
-    // console.log(req.validationErrors());
     if(errores){
-        // console.log(errores);
         req.flash('error',errores.map(err=>err.msg));
         res.render('crear-cuenta',{
         nombrePagina:'Crear tu cuenta en devJobos',
@@ -44,5 +29,21 @@ exports.validarRegistro=(req,res,next)=>{
         return;
     }
     next();
-    return;
+}
+
+exports.crearUsuario=async(req,res,next)=>{
+    const usuario=new Usuarios(req.body);
+    const{nombre,email}=req.body;
+    try{
+        await usuario.save();
+        res.redirect('/iniciar-sesion');
+    }catch(error){
+        req.flash('error',error);
+        // res.redirect('/crear-cuenta');
+        res.render('crear-cuenta',{
+            nombrePagina:'Crear tu cuenta en devJobos',
+            tagline:'Comienza a publicar tus vacantes gratis, solo debes crear una cuenta',
+            mensajes:req.flash()
+        })
+    }
 }
--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(flash());
 app.use((req,res,next)=>{
     res.locals.mesajes=req.flash();
     next();
-})
+});
 app.use('/',router());
 
 app.listen(process.env.PUERTO,()=>{
